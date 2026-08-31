b8c7f73099f725e2404c8d2602a77ac63717ec69
hermani <hermani457@gmail.com>
2026-08-31 14:14:00 -0400

    chore: include NODE_ENV in server startup log message

--- src/index.ts
+++ src/index.ts
@@ -18,5 +18,5 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.listen(PORT, () => {
-  logger.info(`Server is running on port ${PORT}`);
+  logger.info(`Server running on port ${PORT} [${process.env.NODE_ENV ?? 'development'}]`);
 });
